beb1cde10a4e0f767449ee5bb31c5206a26e90d4
Willian Damasceno <williandamascenoprofessional@gmail.com>
2022-02-23 21:21:41 -0300

    Client initial GraphQL queries setup

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,9 +1,31 @@
 import { gql } from '@apollo/client'
 
-export const currencyQuery = gql`
-	query GetRates {
-		rates(currency: "USD") {
-			currency
+export const CLIENT_NAME_BY_SENSITIVE_INFO = gql`
+	query getSensitiveInfoByEmail($email: String!, $password: String!) {
+		clients(where: { email: $email, password: $password }) {
+			firstName
+			authToken
+		}
+	}
+`
+
+export const PUBLIC_CLIENT_URL_NANE = gql`
+	query getPublicUrl($email: String!) {
+		clients(where: { email: $email }) {
+			publicUrlName
+		}
+	}
+`
+
+export const REGISTERED_LINKS = gql`
+	query getAllRegisteredLinks($email: String!) {
+		clients(where: { email: $email }) {
+			links {
+				title
+				url
+				active
+				type
+			}
 		}
 	}
 `
