56f3cfec7c2182ab71924e543929dca9efa9cf2a
Willian Damasceno <williandamascenoprofessional@gmail.com>
2022-03-12 16:36:27 -0300

    Register and Login page design change
    Password confirmation
    Public Url Validation

--- pages/auth/login.jsx
+++ pages/auth/login.jsx
@@ -9,7 +9,9 @@ const Login = () => {
 			</Head>
 
 			<main className='grid place-items-center min-h-screen p-4 md:p-8 bg-gray-100 accent-purple-600'>
-				<form className='grid gap-4 w-full max-w-md h-max p-8 md:p-12 rounded-3xl bg-white shadow-lg'>
+				<form className='grid gap-4 w-full max-w-lg h-max p-8 md:p-12 rounded-3xl bg-white shadow-xl'>
+					<h1 className='text-4xl'>Login</h1>
+
 					<div>
 						<label htmlFor='login-input-email' className='inline-block px-4 py-2 cursor-pointer'>
 							E-mail
@@ -17,8 +19,10 @@ const Login = () => {
 						<input
 							required
 							type='email'
+							tabIndex='1'
 							id='login-input-email'
 							placeholder='E-mail'
+							title='Type your Registered E-mail'
 							className='block w-full p-4 border border-gray-300 rounded-md'
 						/>
 					</div>
@@ -32,22 +36,29 @@ const Login = () => {
 							type='password'
 							id='login-input-password'
 							placeholder='Password'
+							title='Type your Password'
 							className='block w-full p-4 border border-gray-300 rounded-md'
 						/>
 					</div>
 
-					<div>
+					<div title='Click to Login Automatically'>
 						<input type='checkbox' id='stay-signed-in' className='w-max mr-2 cursor-pointer' />
-						<label htmlFor='stay-signed-in' className='cursor-pointer'>Stay signed in</label>
+						<label htmlFor='stay-signed-in' className='cursor-pointer'>
+							Stay signed in
+						</label>
 					</div>
 
-					<button className='w-max py-4 px-8 rounded-md text-white bg-purple-600 hover:bg-purple-700 active:bg-purple-600 transition'>
-						Login
-					</button>
+					<div className='flex gap-2'>
+						<button className='w-max py-4 px-8 rounded-md text-white bg-purple-600 hover:bg-purple-700 active:bg-purple-600 outline-offset-2 accent-slate-700 transition'>
+							Login
+						</button>
 
-					<Link href='/auth/register'>
-						<a className='w-max text-purple-800 hover:text-purple-500 underline'>Create an account</a>
-					</Link>
+						<Link href='/auth/register'>
+							<button className='w-fit px-4 rounded-md text-purple-800 hover:text-white active:text-white hover:bg-gray-500 outline-offset-2 active:bg-gray-600 transition'>
+								Create an account
+							</button>
+						</Link>
+					</div>
 				</form>
 			</main>
 		</>
--- pages/auth/register.jsx
+++ pages/auth/register.jsx
@@ -1,7 +1,38 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import { useState, useRef } from 'react'
 
 const Register = () => {
+	const validUrlDigits = [...new Set('abcdefghijklmnopqrstuvwxyz0123456789-')]
+	const publicUrlBase = 'ioniclink.com/'
+
+	const [publicUrl, setPublicUrl] = useState(publicUrlBase)
+	const [hasValidPublicUrl, setHasValidPublicUrl] = useState(true)
+
+	const [isConfirmedPassword, setIsConfirmedPassword] = useState(true)
+	const passwordInput = useRef(null)
+
+	const isValidDomainName = (domainName, validDigits) => {
+		const isValid = domainName
+			.toLowerCase()
+			.replaceAll(' ', '-')
+			.split('')
+			.every(domainDigit => validDigits.includes(domainDigit))
+
+		return isValid
+	}
+
+	const renderInvalidDomainOrBrandNameError = () => {
+		setHasValidPublicUrl(false)
+	}
+
+	const getPublicUrl = (baseUrl, domainOrBrandName) => {
+		isValidDomainName(domainOrBrandName, validUrlDigits)
+			? (setHasValidPublicUrl(true),
+			  setPublicUrl(`${baseUrl}${domainOrBrandName.toLowerCase().replaceAll(' ', '-')}`))
+			: renderInvalidDomainOrBrandNameError()
+	}
+
 	return (
 		<>
 			<Head>
@@ -10,22 +41,31 @@ const Register = () => {
 
 			<main className='grid place-items-center min-h-screen p-4 md:p-8 bg-gray-100 accent-purple-600'>
 				<form className='grid gap-4 w-full max-w-4xl h-max p-8 md:p-12 rounded-3xl bg-white shadow-lg'>
-					<div>
-						<label htmlFor='register-input-first-name' className='inline-block px-4 py-2 cursor-pointer'>
-							First Name
-						</label>
-						<input
-							required
-							type='text'
-							id='register-input-first-name'
-							placeholder='First Name'
-							className='block w-full p-4 border border-gray-300 rounded-md'
-						/>
-					</div>
-					
+					<h1 className='text-4xl'>Sign Up</h1>
+
 					<div className='grid md:grid-cols-2 gap-4'>
 						<div>
-							<label htmlFor='register-input-email' className='inline-block px-4 py-2 cursor-pointer'>
+							<label
+								htmlFor='register-input-first-name'
+								className='inline-block px-4 py-2 cursor-pointer'
+							>
+								First Name
+							</label>
+							<input
+								required
+								type='text'
+								tabIndex='1'
+								id='register-input-first-name'
+								placeholder='First Name'
+								className='block w-full p-4 border border-gray-300 rounded-md'
+							/>
+						</div>
+
+						<div>
+							<label
+								htmlFor='register-input-email'
+								className='inline-block px-4 py-2 cursor-pointer'
+							>
 								E-mail
 							</label>
 							<input
@@ -38,46 +78,110 @@ const Register = () => {
 						</div>
 
 						<div>
-							<label htmlFor='register-input-password' className='inline-block px-4 py-2 cursor-pointer'>
+							<label
+								htmlFor='register-input-password'
+								className='inline-block px-4 py-2 cursor-pointer'
+							>
 								Password
 							</label>
 							<input
 								required
 								type='password'
+								minLength={8}
+								ref={passwordInput}
 								id='register-input-password'
 								placeholder='Password'
 								className='block w-full p-4 border border-gray-300 rounded-md'
 							/>
 						</div>
+
+						<div>
+							<label
+								htmlFor='register-input-password'
+								className='inline-block px-4 py-2 cursor-pointer'
+							>
+								Confirm your Password
+							</label>
+							<input
+								required
+								type='password'
+								minLength={8}
+								id='register-input-password'
+								placeholder='Confirm your Password'
+								onBlur={({ target }) => {
+									const { value: password } = passwordInput.current
+									const { value: passwordConfirmation } = target
+
+									password === passwordConfirmation || !passwordConfirmation
+										? setIsConfirmedPassword(true)
+										: setIsConfirmedPassword(false)
+								}}
+								className='block w-full p-4 border border-gray-300 rounded-md'
+							/>
+						</div>
 					</div>
-					
+
+					<div
+						className={`${
+							isConfirmedPassword ? 'hidden' : ''
+						} p-4 rounded-md text-red-700 font-semibold bg-red-400`}
+					>
+						The passwords does not match
+					</div>
+
 					<div className='grid md:grid-cols-2 gap-4'>
 						<div>
-							<label htmlFor='register-input-public-url-name' className='inline-block px-4 py-2 cursor-pointer'>
-								Public URL Name
+							<label
+								htmlFor='register-input-domain-name'
+								className='inline-block px-4 py-2 cursor-pointer'
+							>
+								Your Domain or Brand Name
 							</label>
 							<input
 								required
 								type='text'
-								id='register-input-public-url-name'
-								placeholder='Public URL Name'
+								id='register-input-domain-name'
+								placeholder='Domain or Brand'
+								onInput={({ target }) => getPublicUrl(publicUrlBase, target.value)}
 								className='block w-full p-4 border border-gray-300 rounded-md'
 							/>
 						</div>
 
 						<div>
-							<label htmlFor="register-output-public-url-name" className='inline-block px-4 py-2 cursor-pointer'>This will be your public URL</label>
-							<output id='register-output-public-url-name' className='block w-full p-4 border border-gray-300 rounded-md'>ioniclink.com/</output>
+							<label
+								htmlFor='register-output-public-url-name'
+								className='inline-block px-4 py-2 cursor-pointer'
+							>
+								This will be your Public URL
+							</label>
+							<output
+								id='register-output-public-url-name'
+								className='block w-full p-4 border border-gray-300 rounded-md overflow-x-hidden'
+							>
+								{publicUrl}
+							</output>
 						</div>
 					</div>
 
-					<button className='w-max py-4 px-8 rounded-md text-white bg-purple-600 hover:bg-purple-700 active:bg-purple-600 transition'>
-						Register
-					</button>
+					<div
+						className={`${
+							hasValidPublicUrl ? 'hidden' : ''
+						} p-4 rounded-md text-red-700 font-semibold bg-red-400`}
+					>
+						Invalid Domain or Brand Name
+					</div>
 
-					<Link href='/auth/register'>
-						<a className='w-max text-purple-800 hover:text-purple-500 underline'>Register with my account</a>
-					</Link>
+					<div className='flex gap-2'>
+						<button className='w-max py-4 px-6 md:px-8 rounded-md text-white bg-purple-600 hover:bg-purple-700 active:bg-purple-600 outline-offset-2 accent-slate-700 transition'>
+							Register
+						</button>
+
+						<Link href='/auth/login'>
+							<button className='w-fit py-4 px-3 md:px-4 rounded-md text-purple-800 hover:text-white active:text-white hover:bg-gray-500 outline-offset-2 active:bg-gray-600 transition'>
+								Login with my account
+							</button>
+						</Link>
+					</div>
 				</form>
 			</main>
 		</>
