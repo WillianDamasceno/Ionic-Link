4be4c590d76b8898dfc91d6e4a14effa24211618
Willian Damasceno <williandamascenoprofessional@gmail.com>
2022-03-18 19:06:48 -0300

    Using the GraphCMS token for authorization

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -1,3 +1,10 @@
 import { GraphQLClient } from 'graphql-request';
 
-export const gcms = new GraphQLClient(process.env.GRAPHCMS_ENDPOINT)
+export const gcms = new GraphQLClient(
+  process.env.GRAPHCMS_ENDPOINT,
+  {
+    headers: {
+      authorization: `Bearer ${process.env.GRAPHCMS_PRODUCTION_TOKEN}`
+    }
+  }
+)
