3fca4125c43e9b0fd88cfc44c617be7a0802bfd3
Willian Damasceno <williandamascenoprofessional@gmail.com>
2022-05-01 17:04:17 -0300

    Adding types to validations.ts file

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -1,7 +1,7 @@
 const validDomainCharacters = [...new Set('abcdefghijklmnopqrstuvwxyz0123456789-')]
 
-export const isValidDomainName = (domainName: string) => {
-	const isValid = domainName
+export const isValidDomainName = (domainName: string): boolean => {
+	const isValid: boolean = domainName
 		.toLowerCase()
 		.replaceAll(' ', '-')
 		.split('')
