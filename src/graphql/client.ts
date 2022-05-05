4ed095965b0a50f257d76ebdfaa88f8a0a9c86cb
Willian Damasceno <williandamascenoprofessional@gmail.com>
2022-05-04 21:12:07 -0300

    Abstracting the way the implementation of the api client function should be done

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -3,32 +3,27 @@ import { GraphQLClient } from 'graphql-request'
 export const gcms = new GraphQLClient(String(process.env.GRAPHCMS_ENDPOINT))
 
 const commonHeaders = {
-	method: 'POST',
 	headers: {
 		Accept: 'application/json',
 		'Content-Type': 'application/json',
 	},
 }
 
-export const getClientToken = async (email: string, password: string) => (
-	await fetch('/api/auth/login', {
+const connectOnApi = async (route: string, variables: object) => {
+	const res = await fetch(route, {
+		method: 'POST',
 		...commonHeaders,
-		body: JSON.stringify({
-			email,
-			password,
-		}),
+		body: JSON.stringify(variables)
 	})
-).json()
+
+	return res.json()
+}
+
+export const getClientToken = async (email: string, password: string) =>
+	connectOnApi('/api/auth/login', { email, password })
 
 export const getRegisteredLinks = async (authToken: string) => {
-	const linkFetch = await (
-		await fetch('/api/links', {
-			...commonHeaders,
-			body: JSON.stringify({
-				authToken,
-			}),
-		})
-	).json()
+	const linkFetch = await connectOnApi('/api/links', { authToken })
 
 	const linkResponse = linkFetch.success ? linkFetch.response.registeredLinks : []
 
