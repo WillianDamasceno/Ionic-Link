6e028011bd026181a18ba8a15a8e95f2477ad357
Willian Damasceno <williandamascenoprofessional@gmail.com>
2022-06-10 20:18:16 -0300

    Implementing an error handling to the getSavedUserInfo

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -8,7 +8,12 @@ export const saveUserInfo = ({ email, authToken, stayConnected }: UserAuthInfo) 
 	localStorage.setItem('userAuthInfo', JSON.stringify({ email, authToken, stayConnected }))
 }
 
-export const getSavedUserInfo = (): UserAuthInfo | null =>
-	JSON.parse(String(localStorage.getItem('userAuthInfo') || String(null)))
+export const getSavedUserInfo = (): UserAuthInfo | null => {
+	try {
+		return JSON.parse(String(localStorage.getItem('userAuthInfo') || JSON.stringify('')))
+	} catch {
+		return null
+	}
+}
 
 export const createAuthToken = (): string => String(Math.random()).slice(2)
