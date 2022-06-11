0dd6971c6f06bb1c65c01ebd32bc5b2ba000bddf
Willian Damasceno <williandamascenoprofessional@gmail.com>
2022-06-11 20:30:34 -0300

    Refactoring login api route to use http only cookies to store the user JWT token

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -4,39 +4,42 @@ import NextCors from 'nextjs-cors'
 import { CLIENT_TOKEN_BY_EMAIL } from '../../../src/graphql/queries'
 import { UPDATE_NON_PUBLISHED_USER_AUTH_TOKEN } from '../../../src/graphql/mutations'
 import { gcms } from '../../../src/graphql/client'
-import { createAuthToken } from '../../../src/utils/auth'
+import { createJwtToken, serializeHttpOnlyCookie } from '../../../src/utils/auth'
 
-const getClientInfoFromCms = async (email:string, password:string) => {
+const secretKey = process.env.JWT_SECRET_KEY || ''
+
+const isExistentUser = async (email: string, password: string) => {
 	try {
 		const data = await gcms.request(CLIENT_TOKEN_BY_EMAIL, {
 			email,
 			password,
 		})
 
-		const {
-			clients: [clientSensitiveInfo],
-		} = data
-		const { authToken, username } = clientSensitiveInfo
+		const username = data?.clients?.[0]?.username
+		const userExists = Boolean(username)
 
-		clientSensitiveInfo.authToken = authToken || createAuthToken()
-
-		if (clientSensitiveInfo.authToken !== authToken) {
-			await gcms.request(UPDATE_NON_PUBLISHED_USER_AUTH_TOKEN, {
-				email,
-				username,
-				authToken,
-			})
+		return {
+			userExists,
+			username
 		}
-
-		return clientSensitiveInfo
-	} catch (error) {
-		console.error(error) // TODO: Find a way to keep the errors somewhere
-		return false
+	} catch (e) {
+		console.log({ isExistentUserFunctionError: e })
+		return {
+			userExists: false,
+			username: ''
+		}
 	}
 }
 
-// eslint-disable-next-line consistent-return
-const login = async (req:NextApiRequest, res:NextApiResponse) => {
+const updateUserAuthToken = async (email: string, username: string, authToken: string) => {
+	await gcms.request(UPDATE_NON_PUBLISHED_USER_AUTH_TOKEN, {
+		email,
+		username,
+		authToken,
+	})
+}
+
+const login = async (req: NextApiRequest, res: NextApiResponse) => {
 	await NextCors(req, res, {
 		optionsSuccessStatus: 200,
 	})
@@ -47,12 +50,41 @@ const login = async (req:NextApiRequest, res:NextApiResponse) => {
 
 	const { email, password } = req.body
 
-	const clientSensitiveInfo = await getClientInfoFromCms(email, password)
+	if (!(email || password)) {
+		return res.status(401).json({
+			success: false,
+			response: {
+				message: 'Can not perform login because information are missing'
+			}
+		})
+	}
 
-	if (clientSensitiveInfo) {
-		res.status(200).json({ success: true, response: clientSensitiveInfo })
+	const { userExists, username } = await isExistentUser(email, password)
+
+	if (userExists) {
+		const jwtToken = createJwtToken(username, secretKey)
+		const serializedCookie = serializeHttpOnlyCookie('jwtToken', jwtToken)
+
+		await updateUserAuthToken(email, username, jwtToken)
+
+		res.setHeader('Set-Cookie', serializedCookie)
+		res.status(200).json({
+			success: true,
+			response: {
+				username,
+				isLoggedIn: true
+			}
+		})
 	} else {
-		res.status(404).json({ success: false, response: 'The e-mail or the password is incorrect' })
+		res.status(404).json({
+			success: false,
+			response: {
+				error: {
+					message: 'The e-mail or the password is incorrect'
+				},
+				isLoggedIn: false
+			}
+		})
 	}
 }
 
