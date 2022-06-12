cb6b2497c7c0c4eb4fb795dc88a7585e95da82e1
Willian Damasceno <williandamascenoprofessional@gmail.com>
2022-06-12 17:50:37 -0300

    Formating the mutations.ts file

--- src/graphql/mutations.ts
+++ src/graphql/mutations.ts
@@ -2,9 +2,7 @@ import { gql } from 'graphql-request'
 
 export const NEW_CLIENT = gql`
 	mutation createNewClient($email: String!, $username: String!, $password: String!, $authToken: String!) {
-		createClient (
-			data: { email: $email, username: $username, password: $password, authToken: $authToken }
-		) {
+		createClient(data: { email: $email, username: $username, password: $password, authToken: $authToken }) {
 			email
 			authToken
 			username
@@ -22,13 +20,10 @@ export const PUBLISH_CLIENT = gql`
 
 export const UPDATE_PUBLISHED_USER_AUTH_TOKEN = gql`
 	mutation UpdatePublishedUserAuthToken($authToken: String!, $username: String!, $email: String!) {
-		updateClient(
-			data: {authToken: $authToken}
-			where: {username: $username, email: $email}
-		) {
+		updateClient(data: { authToken: $authToken }, where: { username: $username, email: $email }) {
 			authToken
 		}
-		publishClient(where: {username: $username}) {
+		publishClient(where: { username: $username }) {
 			authToken
 		}
 	}
@@ -36,10 +31,7 @@ export const UPDATE_PUBLISHED_USER_AUTH_TOKEN = gql`
 
 export const UPDATE_NON_PUBLISHED_USER_AUTH_TOKEN = gql`
 	mutation UpdateNonPublishedUserAuthToken($authToken: String!, $username: String!, $email: String!) {
-		updateClient(
-			data: {authToken: $authToken}
-			where: {username: $username, email: $email}
-		) {
+		updateClient(data: { authToken: $authToken }, where: { username: $username, email: $email }) {
 			authToken
 		}
 	}
@@ -47,7 +39,7 @@ export const UPDATE_NON_PUBLISHED_USER_AUTH_TOKEN = gql`
 
 export const CREATE_LINK = gql`
 	mutation CreateLink($title: String!, $url: String!, $clientIdentifier: ClientCreateOneInlineInput!) {
-		createLink(data: {title: $title, url: $url, active: true, client: $clientIdentifier}) {
+		createLink(data: { title: $title, url: $url, active: true, client: $clientIdentifier }) {
 			id
 			title
 			url
@@ -58,7 +50,7 @@ export const CREATE_LINK = gql`
 
 export const PUBLISH_LINK = gql`
 	mutation PublishLink($linkId: ID!) {
-		publishLink(where: {id: $linkId}) {
+		publishLink(where: { id: $linkId }) {
 			id
 			title
 		}
